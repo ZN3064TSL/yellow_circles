735ae094058f23a4cc6bdcf0a2bbb0738122a006
ZN3064TSL <arikp8357@gmail.com>
2023-12-10 22:28:24 +0700

    готовое задание

---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+import random
+
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+
+
+class Window(QWidget):
+    '''Класс окна программы'''
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        '''Метод предназначен для работы QPainter'''
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawCircle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        '''Слот реализует работу кнопки self.pushButton'''
+        self.do_paint = True
+        self.update()
+
+    def drawCircle(self, qp):
+        '''Метод рисует окружности при нажатии self.pushButton'''
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randint(0, 509)
+        y = random.randint(0, 509)
+        size = random.randint(0, 250)
+        x2 = x + size
+        y2 = x + size
+        qp.drawEllipse(x, y, x2, y2)
+
+
+app = QApplication(sys.argv)
+if __name__ == '__main__':
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec())
